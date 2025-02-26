1e881a585cc1f6f1c6e80cb49ec2be120f0aae52
JebrilCzar <jgmacanas@up.edu.ph>
2025-02-26 11:56:04 +0800

    added sample data

---
+++ test.js
@@ -0,0 +1,6 @@
+import accs from "./index.js";
+
+console.log(accs.addAccount(["asd", "sada", "aturing@w3c.com", "dsa"]));
+console.log(accs.addAccount(["Alan", "Turing", "aturing@w3c.com", 58]));
+console.log(accs.addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]));
+console.log(accs.addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]));
