d6ef4e7820183b3224ab6e7e01b10f69cc5aac9a
JebrilCzar <jgmacanas@up.edu.ph>
2025-02-26 10:56:08 +0800

    added generateUniqueID func

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("asd");
+import { v4 as uuidv4 } from "uuid";
+
+function generateUniqueID(firstname, lastname) {
+  //since the uuifv4 function generates multiple unique strings, split function was used to separate the strings by the -
+  var randomid = uuidv4().split("-");
+
+  //tolowercase functions was used to transform the first characters of the first and last name to lower case and add it to the first index of the randomid variable
+  return firstname[0].toLowerCase() + lastname[0].toLowerCase() + randomid[0];
+}
+
+//test
+console.log(generateUniqueID("juan", "delacruz"));
+console.log(generateUniqueID("Alan", "Turing"));
